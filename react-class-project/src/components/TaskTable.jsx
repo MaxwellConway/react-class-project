3cb888b45612e665a6a941d15d2f2665dbb4a5d4
MaxwellConway <maxconway23@gmail.com>
2023-07-06 09:01:42 -0400

    added some colors to task table

--- react-class-project/src/components/TaskTable.jsx
+++ react-class-project/src/components/TaskTable.jsx
@@ -6,6 +6,7 @@ import {
   flexRender,
 } from "@tanstack/react-table";
 import { useMemo } from "react";
+import "./table.css";
 
 const TaskTable = ({ tasks }) => {
   const data = useMemo(() => tasks, []);
@@ -37,36 +38,52 @@ const TaskTable = ({ tasks }) => {
     getCoreRowModel: getCoreRowModel(),
   });
 
+  console.log(table.getRowModel());
+
   return (
     <div className="task-table">
-      <h1>HI!</h1>
-      <table className="table-auto w-full">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <h1 className="header">Hello</h1>
+      <div className="overflow-x-auto">
+        <table className="w-full bg-white border divide-y divide-gray-200">
+          <thead className="bg-gray-50">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    key={cell.id}
+                    id={`State-${row.original.state}`}
+                    className="px-6 py-4 whitespace-nowrap text-sm text-gray-500"
+                  >
+                    <span>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </span>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
